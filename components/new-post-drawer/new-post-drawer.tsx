0bb2b3fa41faa351f9f264cc6d075bf275e17eb6
rezonmain <adiazgomez101@gmail.com>
2024-05-06 15:17:39 -0700

    feat(newPost): add state according to location result

--- components/new-post-drawer/new-post-drawer.tsx
+++ components/new-post-drawer/new-post-drawer.tsx
@@ -1,4 +1,5 @@
 import { type Dispatch, type SetStateAction } from "react";
+import { useRouter } from "next/navigation";
 import {
   DrawerContent,
   DrawerHeader,
@@ -9,6 +10,10 @@ import {
   Drawer,
 } from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
+import { useGeolocation } from "@uidotdev/usehooks";
+import { NewPostForm } from "../new-post-form/new-post-form";
+import { nil } from "@rezonmain/utils-nil";
+import { Skeleton } from "../ui/skeleton";
 
 type NewPostDrawerProps = {
   isOpen: boolean;
@@ -19,7 +24,10 @@ const NewPostDrawer: React.FC<NewPostDrawerProps> = ({
   isOpen,
   onOpenChange,
 }) => {
-  return (
+  const { latitude, longitude, loading } = useGeolocation();
+  const router = useRouter();
+
+  if (loading) {
     <Drawer open={isOpen} onOpenChange={onOpenChange}>
       <DrawerContent>
         <DrawerHeader>
@@ -28,6 +36,47 @@ const NewPostDrawer: React.FC<NewPostDrawerProps> = ({
             A new Flare will be placed on your current location
           </DrawerDescription>
         </DrawerHeader>
+        <div className="flex flex-col space-y-3">
+          <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[200px]" />
+          </div>
+        </div>
+        <DrawerFooter>
+          <DrawerClose>
+            <Button variant="outline">CANCEL</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>;
+  }
+
+  if (nil(latitude) || nil(longitude)) {
+    return (
+      <Drawer open={isOpen} onOpenChange={onOpenChange}>
+        <DrawerContent>
+          <DrawerDescription className="text-center p-10">
+            Flare encountered an error trying to get you location
+          </DrawerDescription>
+          <DrawerFooter>
+            <Button onClick={router.refresh}>RELOAD</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
+  return (
+    <Drawer open={isOpen} onOpenChange={onOpenChange}>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle className="text-left">Create a Flare</DrawerTitle>
+          <DrawerDescription className="text-left">
+            A new Flare will be placed on your current location
+          </DrawerDescription>
+        </DrawerHeader>
+        <NewPostForm location={{ lat: latitude, lng: longitude }} />
         <DrawerFooter>
           <Button>POST</Button>
           <DrawerClose>
