b887a850592b580616ab42e88609010c4fd19d77
rezonmain <adiazgomez101@gmail.com>
2024-05-07 18:43:32 -0700

    feat(ui): add sonner

--- app/_helpers/providers.tsx
+++ app/_helpers/providers.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { Toaster } from "@/components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 function Providers({ children }: React.PropsWithChildren) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <Toaster />
+    </QueryClientProvider>
   );
 }
 
