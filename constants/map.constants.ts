9eec8a871bb5ac5020367682be96e0dfe81b7de9
rezonmain <adiazgomez101@gmail.com>
2024-05-08 22:31:05 -0700

    feat(map): update id, style

--- constants/map.constants.ts
+++ constants/map.constants.ts
@@ -1,6 +1,7 @@
 import { MapCapabilities } from "@/constants/map.enum";
 
-const MAP_ID = "8e0a97af9386fef";
+const _MAP_ID = "8e0a97af9386fef";
+const MAP_ID = "739af084373f96fe";
 
 const BASE_CAPABILITIES = [
   MapCapabilities.FLARE_ADD,
