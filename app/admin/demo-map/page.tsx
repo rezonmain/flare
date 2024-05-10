18a1ba4aa39b8a4e25227d8b54842d8e5467e739
rezonmain <adiazgomez101@gmail.com>
2024-05-09 18:59:24 -0700

    feat(admin): pass admin capabilities

--- app/admin/demo-map/page.tsx
+++ app/admin/demo-map/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { getFlares } from "@/db/queries";
 import { FlareMap } from "@/components/flare-map/flare-map";
+import { ADMIN_CAPABILITIES } from "@/constants/map.constants";
 
 export default async function AdminDemoMap() {
   const flares = await getFlares();
@@ -10,6 +11,7 @@ export default async function AdminDemoMap() {
         defaultCenter={{ lat: 32.5596, lng: -116.6189 }}
         defaultZoom={14}
         flares={flares}
+        capabilities={ADMIN_CAPABILITIES}
       />
     </Suspense>
   );
