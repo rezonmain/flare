ca26f7dd130af9816c972fc62cc33c6d2061ccaf
rezonmain <adiazgomez101@gmail.com>
2024-05-09 18:58:50 -0700

    feat(layout): add session provider

--- app/_helpers/providers.tsx
+++ app/_helpers/providers.tsx
@@ -1,14 +1,19 @@
 "use client";
 import { Provider as JotaiProvider } from "jotai";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 
 const queryClient = new QueryClient();
 
 function Providers({ children }: React.PropsWithChildren) {
   return (
-    <JotaiProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </JotaiProvider>
+    <SessionProvider>
+      <JotaiProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </JotaiProvider>
+    </SessionProvider>
   );
 }
 
