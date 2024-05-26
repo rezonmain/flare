e7854db0acdd2acea9a33b2957bb6d44c4fc44d5
Alejandro Díaz <adiazgomez101@gmail.com>
2024-05-25 17:36:49 -0700

    feat(mapview): add flare title (#4)

--- app/near-me/page.tsx
+++ app/near-me/page.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react";
+import Link from "next/link";
 import { FlareMap } from "@/components/flare-map/flare-map";
 import { getFlares } from "@/db/queries";
 import { BASE_CAPABILITIES } from "@/constants/map.constants";
@@ -8,8 +9,11 @@ export default async function NearMePage() {
   const flares = await getFlares();
   return (
     <Suspense>
+      <h1 className="text-3xl font-bold fixed top-4 left-4 z-10 opacity-50">
+        <Link href="/">Flare</Link>
+      </h1>
       <FlareMap
-        defaultCenter={{ lat: 32.5596, lng: -116.6189 }}
+        defaultCenter={{ lat: 32.57182, lng: -116.633 }}
         defaultZoom={14}
         flares={flares}
         capabilities={BASE_CAPABILITIES}
